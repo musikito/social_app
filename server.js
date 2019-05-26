e7f7db6be2c535d53a431ae049dc83b522a6df43
jose marte <tumusicalatina@gmail.com>
2019-05-26 17:55:13 -0400

    Added Routes

--- server.js
+++ server.js
@@ -8,4 +8,10 @@ connectDB();
 
 app.get("/", (req, res) => res.send("APP running"));
 
+//define the routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
