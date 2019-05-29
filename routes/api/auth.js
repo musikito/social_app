4c20bbddf90d7c36cc29b948103ff001223f22b3
jose marte <tumusicalatina@gmail.com>
2019-05-29 16:19:17 -0400

    User auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,88 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator/check");
 
 /**
  * @route   GET api/auth
  * @desc    Test route
  * @access  Public
  */
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    //send the user info
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error!");
+  }
+});
+
+/**
+ * @route   POST api/auth
+ * @desc    Authenticate a usr and get token
+ * @access  Public
+ */
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    //if errors are found return a bad request(400) and show the error(s)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //deconstruct the req.body, use DRY
+    const { email, password } = req.body;
+    try {
+      //check user exist?
+      let user = await User.findOne({ email });
+      //if exists send a bad request to the page
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      //match user with credentials
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      //return jsonwebtoken
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (error, token) => {
+          if (error) throw error;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error!");
+    }
+  }
+);
 
 module.exports = router;
