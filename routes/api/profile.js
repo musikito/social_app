ce581a24788884aa95253b0149476a2d4e79b10c
jose marte <tumusicalatina@gmail.com>
2019-07-01 12:06:01 -0400

    Monday 7-1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 /**
- * @route   GET api/profile
- * @desc    Test route
- * @access  Public
+ * @route   GET api/profile/me
+ * @desc    Get current user profile
+ * @access  Private
  */
-router.get("/", (req, res) => res.send("Profile Route"));
+router.get("/", auth, async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 module.exports = router;
