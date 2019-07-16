52464ba85d628696b6045c16929fe15dd951f3e7
jose marte <tumusicalatina@gmail.com>
2019-07-16 12:13:52 -0400

    Redux

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,19 +5,19 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
+        <Link to="/">
           <i className='fas fa-code' /> DevConnector
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='profiles.html'>Developers</a>
+          <a href='!#'>Developers</a>
         </li>
         <li>
-          <a href='register.html'>Register</a>
-        </li>
+          <Link to="/register">Register</Link>     
+          </li>
         <li>
-          <a href='login.html'>Login</a>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </nav>
